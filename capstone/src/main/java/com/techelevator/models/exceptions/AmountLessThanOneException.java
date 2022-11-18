4cba6c79b736254eb02a57c39b51c5c43bd61de9
Quinn Peel <thomas.peel@yahoo.com>
2022-11-17 21:46:18 -0800

    started work on logger class

--- capstone/src/main/java/com/techelevator/models/exceptions/AmountLessThanOneException.java
+++ capstone/src/main/java/com/techelevator/models/exceptions/AmountLessThanOneException.java
@@ -2,4 +2,20 @@ package com.techelevator.models.exceptions;
 
 public class AmountLessThanOneException extends Exception {
 
+    private int amountEntered;
+
+    public AmountLessThanOneException(String message, int amountEntered){
+
+        super(message);
+        this.amountEntered = amountEntered;
+    }
+
+    @Override
+    public String getMessage(){
+
+        String message = super.getMessage();
+
+        return message + " - Illegal amount entered: " + amountEntered;
+    }
+
 }
--- capstone/src/main/java/com/techelevator/models/file_io/Logger.java
+++ capstone/src/main/java/com/techelevator/models/file_io/Logger.java
@@ -1,4 +1,41 @@
 package com.techelevator.models.file_io;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Logger {
+
+    private static final String FILE_EXTENSION = ".log";
+
+    private String directory;
+
+    //Constructor
+    public Logger(String directory){
+        this.directory = directory;
+    }
+
+    public void logMessage(String message){
+
+        // Sets the fileName to be the date in YYYY-MM-DD format
+        String fileName = LocalDate.now().format(DateTimeFormatter.ISO_DATE);
+        String logFilePath = directory + "/" + fileName + FILE_EXTENSION;
+        File logFile = new File(logFilePath);
+
+        // Try with resources to ensure closure of the log file after writing
+        try(FileWriter fileWriter = new FileWriter(logFile, true);
+            PrintWriter writer = new PrintWriter(fileWriter)){
+
+            // Opens the log file and appends the new message
+            String timeStamp = LocalDate.now().toString();
+            writer.println(timeStamp + " " + message);
+        } catch (IOException ex){
+            //Swallow the exception
+        }
+    }
 }
+
+
